29aeae79745ab74fff691cb6b685bd7c6690fa70
Denis <turchak92@gmail.com>
2022-01-30 17:02:53 +0200

    task 2.6 point 3

--- lib/binary.dart
+++ lib/binary.dart
@@ -2,3 +2,10 @@
 decimalToBinary(number) {
   return number.toRadixString(2);
 }
+
+getString(text) {
+  text = text.replaceAll(new RegExp(r'[^0-9]'), '');
+  List<String> list = text.split('');
+
+  return list;
+}
