170cda6eaebcee060793bbc7be201ff5029ab2a6
Denis <turchak92@gmail.com>
2022-02-13 18:10:09 +0200

    point 1 or 2

--- bin/dart_application_2_6.dart
+++ bin/dart_application_2_6.dart
@@ -1,6 +1,7 @@
 import 'package:dart_application_2_6/binary.dart';
 
 void main() {
-  print(decimalToBinary(32));
-  print('String: ${getString('fnjdsa fdjs a 32e32 432 4234 ')}');
+  delimetersCalculator(15, 20); //1
+  print(decimalToBinary(32)); //2
+  print('String: ${getString('fnjdsa fdjs a 32e32 432 4234 ')}'); //3
 }
--- lib/binary.dart
+++ lib/binary.dart
@@ -11,3 +11,25 @@ getString(text) {
   }
   throw ArgumentError('Text is not String');
 }
+
+delimetersCalculator(int number1, int number2) {
+  print("НОД $number1 и $number2 = ${number1.gcd(number2)}");
+}
+
+mapData() {
+ const List words = [
+    'Слово один',
+    'Слово два',
+    'Слово три',
+    'Слово четыре',
+    'Слово четыре',
+    'Слово пять',
+    'Слово пять',
+    'Слово пять',
+  ];
+
+  final map = <String, int>{};
+
+  //перебрать массив и посчитать сколько раз слово появляется в массиве
+  
+}
